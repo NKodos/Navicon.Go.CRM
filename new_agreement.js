4db779fb0b33d1154299bc3796f6143b0b59be36
NKodos <kodoses@gmail.com>
2021-08-16 14:08:28 +0300

    completed task 2

--- new_agreement.js
+++ new_agreement.js
@@ -1,18 +1,18 @@
 var Navicon = Navicon || {};
+Navicon.nav_agreement = Navicon.nav_agreement || {};
 const creatingFormType = 1;
 const enabledControlsForCreating = ['new_number', 'new_date', 'new_contact', 'new_autoid'];
 
 // Task 1
 // При создании объекта Договор, сразу после открытия карточки доступны для редактирования 
 // поля: номер, дата договора, контакт и модель. Остальные поля - скрыты. 
-// Вкладка с данными по кредиту скрыта.  
+// Вкладка с данными по кредиту скрыта.
 
-
-Navicon.nav_agreement.tas1 = (function () {
+Navicon.nav_agreement.task1 = (function () {
 
     var disableCreationFormAttributes = function (context) {
         let formContext = context.getFormContext();
-        var controls = formContext.getControl();
+        let controls = formContext.getControl();
 
         console.log('Controls:');
 
@@ -23,9 +23,9 @@ Navicon.nav_agreement.tas1 = (function () {
             }
         });
 
-        var tabObj = formContext.ui.tabs.get('Credit');
-        console.log(tabObj);
-        tabObj.setVisible(false);
+        let creditTab = formContext.ui.tabs.get('Credit');
+        console.log(creditTab);
+        creditTab.setVisible(false);
     };
 
     return {
@@ -37,12 +37,43 @@ Navicon.nav_agreement.tas1 = (function () {
             if (formType == creatingFormType) {
                 disableCreationFormAttributes(context);
             }
+        }
+    };
+})();
+
+// Task 2
+// На объекте Договор, после выбора значения в полях контакт и автомобиль, 
+// становиться доступной вкладка кредитная программа.
+
+Navicon.nav_agreement.task2 = (function () {
+
+    var autoAndContactOnChange = function (context) {
+        let formContext = context.getFormContext();
+
+        let contactAttrValue = formContext.getAttribute('new_contact').getValue();
+        let autoidAttrValue = formContext.getAttribute('new_autoid').getValue();
+
+        console.log(`contact=${contactAttrValue}, auto=${autoidAttrValue}`);
+
+        let creditTab = formContext.ui.tabs.get('Credit');
+
+        if (contactAttrValue && autoidAttrValue) {
+            creditTab.setVisible(true);
+        } else {
+            creditTab.setVisible(false);
+        }
+    };
+
+    return {
+        onLoad: function (context) {
+
+            let formContext = context.getFormContext();
 
             let contactAttr = formContext.getAttribute('new_contact');
             let autoidAttr = formContext.getAttribute('new_autoid');
 
-            contactAttr.addOnChange(floorOnChange);
-            autoidAttr.addOnChange(floorOnChange);
+            contactAttr.addOnChange(autoAndContactOnChange);
+            autoidAttr.addOnChange(autoAndContactOnChange);
         }
     };
 })();
