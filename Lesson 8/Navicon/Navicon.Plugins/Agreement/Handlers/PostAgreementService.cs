66ee376ebb352194484044c7661d02ea31c20050
NKodos <kodoses@gmail.com>
2021-08-25 17:04:06 +0300

    complete task 5

---
+++ Lesson 8/Navicon/Navicon.Plugins/Agreement/Handlers/PostAgreementService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+using Navicon.Common.Entities;
+
+namespace Navicon.Plugins.Agreement.Handlers
+{
+    public class PostAgreementService
+    {
+        private readonly IOrganizationService _service;
+
+        public PostAgreementService(IOrganizationService service)
+        {
+            _service = service ?? throw new ArgumentNullException(nameof(service));
+        }
+
+        public void TrySetFact(new_agreement targetAgreement)
+        {
+            var factSumma = targetAgreement.new_factsumma ?? new Money(0);
+            var summa = targetAgreement.new_summa ?? new Money(0);
+
+            if (factSumma.Value == summa.Value)
+            {
+                var updatedAgreement = new new_agreement {Id = targetAgreement.Id, new_fact = true};
+                _service.Update(updatedAgreement);
+            }
+        }
+    }
+}
---
+++ Lesson 8/Navicon/Navicon.Plugins/Agreement/PostAgreementCreate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+using Navicon.Common.Entities;
+using Navicon.Plugins.Agreement.Handlers;
+using Navicon.Plugins.Invoice.Handlers;
+
+namespace Navicon.Plugins.Agreement
+{
+    public sealed class PostAgreementCreate : PluginBase
+    {
+        public override void Execute(IServiceProvider serviceProvider)
+        {
+            var serviceInfo = GetBaseInfo<new_agreement>(serviceProvider);
+
+            try
+            {
+                var service = new PostAgreementService(serviceInfo.OrganizationService);
+                service.TrySetFact(serviceInfo.TargetEntity);
+            }
+            catch (Exception ex)
+            {
+                serviceInfo.TracingService.Trace(ex.ToString());
+                throw new InvalidPluginExecutionException(ex.Message);
+            }
+        }
+    }
+}
