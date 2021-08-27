d7fef8d2be281bf8e9db7afb837531c0bd4a3fb4
NKodos <kodoses@gmail.com>
2021-08-27 20:10:09 +0300

    add workflow project

---
+++ Lesson 8/Navicon/Navicon.Workflow/AgreementActivities/AgreementValidationActivity.cs
@@ -0,0 +1,40 @@
+﻿using System.Activities;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+using Navicon.Common.Entities;
+
+namespace Navicon.Workflow.AgreementActivities
+{
+    public class AgreementValidationActivity : CodeActivity
+    {
+        [Input("Agreement")]
+        [RequiredArgument]
+        [ReferenceTarget("new_agreement")]
+        public InArgument<EntityReference> AgreementReference { get; set; }
+
+        [Output("Is Agreement valid")]
+        public OutArgument<bool> IsValid { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var wfContext = context.GetExtension<IWorkflowContext>();
+            var organizationServiceFactory = context.GetExtension<IOrganizationServiceFactory>();
+            var service = organizationServiceFactory.CreateOrganizationService(null);
+
+            var agreementRef = AgreementReference.Get(context);
+
+            var query = new QueryExpression(new_invoice.EntityLogicalName)
+            {
+                ColumnSet = new ColumnSet(new_invoice.Fields.new_name),
+                NoLock = true,
+                TopCount = 1
+            };
+            query.Criteria.AddCondition(new_invoice.Fields.new_dogovorid, ConditionOperator.Equal, agreementRef.Id);
+            var queryResult = service.RetrieveMultiple(query);
+
+            IsValid.Set(context, queryResult.Entities.Count > 0);
+
+        }
+    }
+}
